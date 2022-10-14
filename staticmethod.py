d8d4b8c228bd4f1cd7d7c03d2196e98415ef0e30
Sneka2002 <111284253+Sneka2002@users.noreply.github.com>
2022-10-14 20:43:52 +0530

    Add files via upload

---
+++ staticmethod.py
@@ -0,0 +1,42 @@
+# static method
+ 
+ class Person:
+    def _init_(self, name, age):
+        self.name = name
+        self.age = age
+
+    @staticmethod
+    def fromFathersAge(name, fatherAge, fatherPersonAgeDiff):
+        return Person(name, date.today().year - fatherAge + fatherPersonAgeDiff)
+
+    @classmethod
+    def fromBirthYear(cls, name, birthYear):
+        return cls(name, date.today().year - birthYear)
+
+    def display(self):
+        print(self.name + "'s age is: " + str(self.age))
+
+class Man(Person):
+    sex = 'Male'
+
+man = Man.fromBirthYear('John', 1985)
+print(isinstance(man, Man))
+
+man1 = Man.fromFathersAge('John', 1965, 20)
+print(isinstance(man1, Man))
+
+# inheritance 
+ 
+ class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+class DogChild(Dog):  
+    def eat(self):  
+        print("Eating bread...")  
+d = DogChild()  
+d.bark()  
+d.speak()  
+d.eat()
